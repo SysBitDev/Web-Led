b384317486ef4a9984ab6a0ac54973e7a053c17b
vtymoshchuk-git <vitaliy.t@techspark.engineering>
2024-10-03 14:27:34 +0300

    Del trash

--- components/http/http.c
+++ components/http/http.c
@@ -105,7 +105,7 @@ static esp_err_t root_get_handler(httpd_req_t *req) {
                                "<head>"
                                "<meta charset='UTF-8'>"
                                "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
-                               "<title>LED Controller - Metro 2033</title>"
+                               "<title>LED Controller</title>"
                                "<link href='https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&family=Roboto+Mono:wght@400;700&display=swap' rel='stylesheet'>"
                                "<style>"
                                "body {"
